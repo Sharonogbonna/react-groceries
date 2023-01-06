8410e86fe9c1b6ec276dace2dd0f6ef978a6ad6f
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2023-01-06 14:32:50 -0600

    completed grocery list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import listArr from "./data";
+import ItemDisplay from "./components/ItemDisplay";
+import "./style.css";
 
 function App() {
+  //setting up use state, states initial value is the array of groceries
+  const [grList, setGrList] = useState(listArr);
+  const addItem = (e) => {
+    e.preventDefault();
+    setGrList(
+      grList.concat([
+        {
+          item: document.querySelector('input[name="item"]').value,
+          brand: document.querySelector('input[name="brand"]').value,
+          units: document.querySelector('input[name="units"]').value,
+          quantity: document.querySelector('input[name="quantity"]').value,
+          isPurchased: false,
+        },
+      ])
+    );
+    console.log(grList);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Form/> */}
+      <h1 className="page-title">My Grocery List</h1>
+      <form onSubmit={addItem}>
+        <h3>Add an Item to Your List</h3>
+        <p>Item:</p> <input type="text" name="item"></input> <br />
+        <p>Brand:</p> <input type="text" name="brand"></input>
+        <br />
+        <p>Units:</p> <input type="text" name="units"></input>
+        <br />
+        <p>Quantity:</p> <input type="number" name="quantity"></input>
+        <br />
+        <input type="submit" value="submit"></input>
+      </form>
+
+      <section className="items">
+        {grList.map((ele, idx) => {
+          return !ele.isPurchased && <ItemDisplay {...ele} key={idx} />;
+        })}
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        item: 'Toilet Paper',
+        brand: 'Charmin',
+        units: '12 pack',
+        quantity: 3,
+        isPurchased: false
+      }, 
+      {
+        item: 'Honey',
+        brand: 'Kelly\'s Local Texas Honey',
+        units: '40 oz',
+        quantity: 2,
+        isPurchased: false
+      }, 
+      {
+        item: 'Cereal',
+        brand: 'Cheerios',
+        units: 'Family Size',
+        quantity: 1,
+        isPurchased: false
+      },
+      {
+        item: 'Salmon',
+        brand: 'Trident',
+        units: '20 oz',
+        quantity: 10,
+        isPurchased: false
+    },
+    {
+        item: 'Asparagus',
+        brand: 'Birds Eye',
+        units: '8 oz',
+        quantity: 5,
+        isPurchased: false
+    },
+    {
+        item: 'Broccoli',
+        brand: 'Green Giant',
+        units: '10 oz',
+        quantity: 15,
+        isPurchased: false
+    }
+]
